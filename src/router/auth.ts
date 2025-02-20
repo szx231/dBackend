c6e22d57f7ab7fb16d5cf77581abafd0929c6167
CyberHazard <cyberhazard@inbox.ru>
2025-02-20 23:16:55 +0300

    Добавить логику аутентификации пользователей с поддержкой сессий

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,15 +1,10 @@
 import * as authController from '@/controllers/auth.js';
 import { FastifyInstance } from 'fastify';
-import { registerUserSchema } from './auth.schemas.js';
+import { logInSchema, registerUserSchema } from './auth.schemas.js';
 
 export const authRouter = async (instance: FastifyInstance) => {
-  instance.post(
-    '/register',
-    {
-      schema: {
-        body: registerUserSchema,
-      },
-    },
-    authController.register,
-  );
+  instance.post('/register', { schema: { body: registerUserSchema } }, authController.register);
+  instance.post('/login', { schema: { body: logInSchema } }, authController.logIn);
+  instance.get('/session', authController.getSession);
+  instance.get('/logout', authController.logOut);
 };
