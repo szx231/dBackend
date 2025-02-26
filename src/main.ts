1015a4422aa5075ee3cd4cef2b011ff533ccb84c
CyberHazard <cyberhazard@inbox.ru>
2025-02-27 01:23:42 +0300

    add redis, connect-redis

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@
 
 import fastifyCookie from '@fastify/cookie';
 import fastifySession from '@fastify/session';
+import { RedisStore } from 'connect-redis';
 import Fastify from 'fastify';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { createClient } from 'redis';
 
 import { authRouter } from './router/auth.js';
 
 const server = Fastify({ logger: true });
+
+const redisClient = createClient({ url: process.env.REDIS_URL });
+redisClient.connect().catch(console.error);
+
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: 'dating:',
+});
 
 server.register(fastifyCookie);
 server.register(fastifySession, {
@@ -15,6 +25,7 @@ server.register(fastifySession, {
   rolling: true,
   saveUninitialized: true,
   cookieName: 'sessionId',
+  store: redisStore,
   cookie: {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 30,
